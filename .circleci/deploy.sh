b7b40085fb0d2d8a99bb08a49d971ff76b9d7392
Rothanak So <rothanak.hello@gmail.com>
2017-10-13 12:24:16 -0400

    Add symbolic link for public storage to deploy.sh

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -7,8 +7,14 @@ if [ ! -d "cocoabay" ]; then
     git clone git@github.com:rothso/cocoabay.git cocoabay
 
     # Symlink the public directory to public_html ("portal" is our subdomain folder)
+    echo "Creating symbolic link from public to www/cocoabay.net/portal"
     cd ~/www/cocoabay.net
     ln -s ~/cocoabay/public portal
+
+    # Symlink the public storage to the public directory
+    echo "Creating symbolic link from public/storage to storage/app/public"
+    cd ~/cocoabay/public
+    ln -s ~/cocoabay/storage/app/public storage
 else
     cd ~/cocoabay
     git pull

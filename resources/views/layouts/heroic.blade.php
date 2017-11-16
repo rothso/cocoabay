0f12398ee931e4c5b34c767383740bfa7e5889ac
Rothanak So <rothanak.hello@gmail.com>
2017-11-16 10:03:08 -0500

    Use a minimalistic DMV background

--- resources/views/layouts/heroic.blade.php
+++ resources/views/layouts/heroic.blade.php
@@ -18,16 +18,11 @@
 
         .hero {
             background:
-                linear-gradient(-45deg,
-                    rgba(16, 156, 133, 1) 0%,
-                    rgba(16, 156, 133, 1) 25%,
-                    rgba(16, 156, 133, 0) 100%
-                ),
                 linear-gradient(
                     rgba(16, 156, 133, 0.85),
                     rgba(16, 156, 133, 0.85)
                 ),
-                url('{{ asset('img/joey-kyber-121699.jpg') }}') no-repeat 0 -415px / cover;
+                url('{{ asset('img/dmv-triangles.png') }}') no-repeat 0 -415px / cover;
             padding-top: 10px;
             margin-bottom: 50px;
             box-shadow: inset 0 -7px 9px -7px rgba(0, 0, 0, 0.15);
